0b4c5a4517b315430dee69c4b63ed34a5e4dcb5b
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-12-06 23:54:17 -0500

    changes from template engine exersice

--- express_server.js
+++ express_server.js
@@ -13,6 +13,18 @@ app.get("/urls.json", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
