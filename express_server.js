5460583d70adf8827fe83a9d8daaec08bcf47ae8
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-12-07 19:44:25 -0500

    EJS templating

--- express_server.js
+++ express_server.js
@@ -8,8 +8,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
 app.get("/", (req, res) => {
@@ -26,6 +34,10 @@ app.get("/set", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
